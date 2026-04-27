73799aed61bbfc70efa91b4f7587b7b76521da1b
elorn-kali-eskrima <elornkalieskrima@gmail.com>
2026-04-27 19:59:53 +0000

    Bouton deco garder juste emoji (ergonomie)

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function AppContent() {
               onClick={signOut}
               className="text-xs bg-white/10 hover:bg-white/20 px-3 py-1.5 rounded transition"
             >
-              🚪 Déconnexion
+              🚪
             </button>
           </div>
         </div>
